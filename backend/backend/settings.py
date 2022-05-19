4ec344c965dbfa67d67cc0c7f6629213fc94146c
Anastasia Krivosheeva <anastasia.krivosheeva.97@gmail.com>
2022-05-19 17:32:12 +0300

    Добавлена комманда для загрузки ингредиентов

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'd2#_14m4*w&q)p5=+l#6)vm8v#%1*=%2r$#lzz0li6v#mb5jj%'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+import csv
+
+
+class CommandLoadData(BaseCommand):
+    """Комманда для загрузки ингредиентов из csv."""
+    help='Load ingredients data'
+
+    def handle(self, *args, **options):
+        with open('data/ingredients.csv') as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
