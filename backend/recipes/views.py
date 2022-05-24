3e5a39ab866fc4cff0cdbf9362157717f6fe4127
Anastasia Krivosheeva <anastasia.krivosheeva.97@gmail.com>
2022-05-24 12:10:13 +0300

    Изменена фильтрация по тэгам

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -11,7 +11,7 @@ from api.serializers import (FavoriteANDShoppingListSerializer,
                              RecipeCreateUpdateSerializer, RecipeGETSerializer,
                              TagSerializer)
 
-from .filters import IngredientSearchFilter
+from .filters import IngredientSearchFilter, TagsFilterSet
 from .models import (FavoriteList, Ingredient, Recipe, RecipeIngredients,
                      ShoppingCart, Tag)
 from .permissions import AuthorOrReadOnly
@@ -34,7 +34,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     permission_classes = (AuthorOrReadOnly,)
-    filter_backends = (DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend, TagsFilterSet)
     filterset_fields = ('author',)
 
     def get_queryset(self):
@@ -43,9 +43,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         is_in_shopping_cart = self.request.query_params.get(
             'is_in_shopping_cart'
         )
-        tags = self.request.query_params.get('tags')
-        if tags:
-            queryset = queryset.filter(tags__slug=tags)
+        # tags = self.request.query_params.get('tags')
+        # if tags:
+        #     queryset = queryset.filter(tags__slug=tags)
         if is_favorited:
             recipes_id = FavoriteList.objects.filter(
                 user=self.request.user
