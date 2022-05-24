c7789ae86ac22c58b668220ced49db3fccad76d1
Anastasia Krivosheeva <anastasia.krivosheeva.97@gmail.com>
2022-05-25 00:36:41 +0300

    Правка фильтрации

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -9,8 +9,8 @@ class IngredientSearchFilter(filters.SearchFilter):
 
 class RecipesFilter(filter.FilterSet):
     tags = filter.ModelMultipleChoiceFilter(
-        #name='tags__slug',
-        to_field_name='slug',
+        field_name='tags__slug',
+        to_field_name='tags',
         #lookup_type='in',
         queryset=Tag.objects.all()
     )
